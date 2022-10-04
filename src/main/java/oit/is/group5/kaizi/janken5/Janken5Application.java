8380651177df6a3fe8f8fc277f818076b569ffb3
e1b20108 <e1b20108@oit.ac.jp>
2022-10-04 14:29:05 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/group5/kaizi/janken5/Janken5Application.java
@@ -0,0 +1,13 @@
+package oit.is.group5.kaizi.janken5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Janken5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Janken5Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/group5/kaizi/janken5/Janken5ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.group5.kaizi.janken5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Janken5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
